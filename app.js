e94e10cb3af946fa1407e912c305a38a14fa8445
yttiiz <dominique.talis@gmail.com>
2022-12-08 17:49:08 +0100

    code review dec.2022 almost done (v4)

--- app.js
+++ app.js
@@ -3,36 +3,32 @@
 // My elements
 const [btnMinus, btnPlus] = document.querySelectorAll('.product-add-to-cart div button')
 const input = document.querySelector('.product-add-to-cart input')
-const btnAdd = document.querySelector('.product-add-to-cart button')
-const svgCart = document.querySelector('#cart > div:first-child')
+const btnAdd = document.querySelector('.product-add-to-cart > button')
+const [svgCart, avatar] = document.querySelectorAll('#cart button')
 const label = svgCart.querySelector('span')
-const avatar = document.querySelector('#cart img')
 const sticker = document.querySelector('header > div:last-child')
 
-// My count & price
+// My count
 let count = 0
-let price = 125.00
-
-// My text
-const emptyText = '<span>Your cart is empty.</span>'
+const price = 125
 
 // My div cart
 const stickerContent = sticker.querySelector('div')
 
 // My functions
-const createStickerContentEmpty = () => stickerContent.innerHTML = emptyText
+const createStickerContentEmpty = () => stickerContent.innerHTML = '<span>Your cart is empty.</span>'
 const toogleStickerClass = () => sticker.classList.toggle('sticker')
 const createElts = (type) => document.createElement(type)
 
 function createStickerContentFilled() {
-    const img =  createElts('img'), span =  createElts('span'), button = createElts('button')
+    const img = createElts('img'), span = createElts('span'), button = createElts('button')
     const divProduct = createElts('div'), divButton = createElts('div')
     
     img.src = 'images/image-product-1-thumbnail.jpg'
     img.alt = 'image product 1'
     divProduct.appendChild(img)
 
-    span.innerHTML = `Fall Limited Edition Sneakers<br/>$125.00 x <span>0</span> <strong>$0</strong>`
+    span.innerHTML = `Fall Limited Edition Sneakers<br/>$${price.toFixed(2)} x <span>0</span> <strong>$0</strong>`
     divProduct.appendChild(span)
     divProduct.innerHTML += `<svg width="14" height="16" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><defs><path d="M0 2.625V1.75C0 1.334.334 1 .75 1h3.5l.294-.584A.741.741 0 0 1 5.213 0h3.571a.75.75 0 0 1 .672.416L9.75 1h3.5c.416 0 .75.334.75.75v.875a.376.376 0 0 1-.375.375H.375A.376.376 0 0 1 0 2.625Zm13 1.75V14.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 1 14.5V4.375C1 4.169 1.169 4 1.375 4h11.25c.206 0 .375.169.375.375ZM4.5 6.5c0-.275-.225-.5-.5-.5s-.5.225-.5.5v7c0 .275.225.5.5.5s.5-.225.5-.5v-7Zm3 0c0-.275-.225-.5-.5-.5s-.5.225-.5.5v7c0 .275.225.5.5.5s.5-.225.5-.5v-7Zm3 0c0-.275-.225-.5-.5-.5s-.5.225-.5.5v7c0 .275.225.5.5.5s.5-.225.5-.5v-7Z" id="a"/></defs><use fill="#C3CAD9" fill-rule="nonzero" xlink:href="#a"/></svg>`
     
@@ -47,7 +43,7 @@ function createStickerContentFilled() {
         
         // On click on svg trash
         svg.addEventListener('click', () => {
-            stickerContent.innerHTML = emptyText
+            createStickerContentEmpty()
             count = 0
             input.value = count
             label.textContent = ''
@@ -56,7 +52,7 @@ function createStickerContentFilled() {
     }
 }
 
-function displayCountAndPrice(count, price) {
+function displayCountAndPrice(count) {
     stickerContent.querySelector('span').children[1].textContent = count
     stickerContent.querySelector('strong').textContent = `$${count * price}.00`
 }
@@ -73,14 +69,7 @@ btnMinus.addEventListener('click', () => {
 
             switch(count) {
                 case 0:
-                    label.textContent = ''
-                    label.classList.remove('cart-count-not-empty')
                     createStickerContentEmpty()
-                    break
-
-                default:
-                    label.textContent = count
-                    displayCountAndPrice(count, price)
             }
     }
 })
@@ -88,19 +77,36 @@ btnMinus.addEventListener('click', () => {
 btnPlus.addEventListener('click', () => {
     count++
     input.value = count
-    label.textContent = count
-    label.classList.add('cart-count-not-empty')
+})
 
-    if (count === 1) {
-        stickerContent.removeChild(stickerContent.childNodes[0])
-        createStickerContentFilled()
+btnAdd.addEventListener('click', () => {
+    const length = stickerContent.children.length
+    
+    if (count > 0) {
+        if (length === 1) {
+            stickerContent.removeChild(stickerContent.children[0])
+            label.textContent = count
+            label.classList.add('cart-count-not-empty')
+            createStickerContentFilled()
+            displayCountAndPrice(count)
+
+        } else displayCountAndPrice(count)
     }
+})
 
-    displayCountAndPrice(count, price)
+// Click out the box to close the sticker cart
+window.addEventListener('click', (e) => {
+    const target = e.target
+
+    if (target?.dataset.name === 'cart-toggle' || target?.dataset.name === 'handle-cart-content') return
+    else if (sticker.classList.contains('sticker')) {
+        sticker.classList.remove('sticker')
+    }
 })
 
 avatar.addEventListener('click', toogleStickerClass)
 svgCart.addEventListener('click', toogleStickerClass)
+sticker.addEventListener('click', (e) => e.stopPropagation())
 
 createStickerContentEmpty()
 
@@ -117,24 +123,26 @@ const length = slider.children.length // number : 4
 let countSlider = 0
 
 // My functions
+const fillImage = (img, number) => img.src = `images/image-product-${number + 1}.jpg`
+const addActiveClass = (item) => item.classList.add('active')
+const removeActiveClass = (item) => item.classList.remove('active')
+const removeAllActiveClass = (num, nodelist) => {
+    num >= length
+        ? removeActiveClass(nodelist[num - length])
+        : removeActiveClass(nodelist[num + length])
+}
+
 function moveSlider(slider, count) {
     let sliderWidth = slider.clientWidth
     slider.style.transform = `translateX(-${(sliderWidth / length) * count}px)`
 }
 
-const fillImage = (img, number) => img.src = `images/image-product-${number + 1}.jpg`
-
 function loopSearchActiveClass(number, list) {
     for (let i = number; i < list.length; i++) {
-        if (list[i].className === 'active'){
-            list[i].classList.remove('active')
-            
-            if (number >= length) {
-                list[i - length].classList.remove('active')
-                
-            } else {
-                list[i + length].classList.remove('active')
-            }
+        
+        if (list[i].className === 'active') {
+            removeActiveClass(list[i])
+            removeAllActiveClass(i, list)
         }
     }
 }
@@ -146,12 +154,12 @@ function handleThumbnails() {
         if (index >= length) {
 
             if (thumbnail.className === 'active') {
-                thumbnail.classList.remove('active')
-                thumbnails[index - length].classList.remove('active')
+                removeActiveClass(thumbnail)
+                removeActiveClass(thumbnails[index - length])
             }
 
-            thumbnails[countSlider + length].classList.add('active')
-            thumbnails[countSlider].classList.add('active')
+            addActiveClass(thumbnails[countSlider + length])
+            addActiveClass(thumbnails[countSlider])
         }
     })
 }
@@ -186,9 +194,10 @@ thumbnailList.forEach((thumbnail, index, thumbnails) => {
             loopSearchActiveClass(length, thumbnails)
             moveSlider(slider, index - length)
 
-            thumbnail.classList.add('active')
-            thumbnails[index - length].classList.add('active')
+            addActiveClass(thumbnail)
+            addActiveClass(thumbnails[index - length])
             countSlider = index - length
+
             fillImage(mainVisual, countSlider)
 
         } else {
@@ -202,8 +211,8 @@ thumbnailList.forEach((thumbnail, index, thumbnails) => {
                 loopSearchActiveClass(0, thumbnails)
                 moveSlider(slider, index)
 
-                thumbnail.classList.add('active')
-                thumbnails[index + length].classList.add('active')
+                addActiveClass(thumbnail)
+                addActiveClass(thumbnails[index + length])
                 countSlider = index
             }
         }
